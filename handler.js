71172d7050e32db17725188c8788a458b2a9809d
Robin Ochoa <robin8a@gmail.com>
2024-06-25 17:57:19 -0500

    Initial commit

---
+++ handler.js
@@ -0,0 +1,10 @@
+import AWS from 'aws-sdk';
+
+exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4! Your function executed successfully!",
+    }),
+  };
+};
